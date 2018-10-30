83277cd538cd86ade2b207862a15915c9712eedf
FADINGLIGHT <FadingLight9291117@users.noreply.github.com>
2018-10-30 17:50:53 +0800

    17.50

--- WebApplication3/Controllers/BlogManageController.cs
+++ WebApplication3/Controllers/BlogManageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication3.EF;
 
 namespace WebApplication3.Controllers
 {
@@ -13,7 +14,35 @@ namespace WebApplication3.Controllers
         public ActionResult Index(FormCollection fc)
         {
             var content = fc["content"];
-            return View();
+
+            List<blog_Table> list = new List<blog_Table>();
+            personal_blogEntities3 db = new personal_blogEntities3();
+            var find = db.blog_Table;
+            foreach (var read in find)
+            {
+
+                list.Add(new blog_Table()
+                {
+                    BlogID = read.BlogID,
+                    Title = read.Title,
+                    Synopsis = read.Synopsis,
+                    Content = read.Content,
+                    Image = read.Image,
+                    DateTime = read.DateTime,
+                    View_number = read.View_number,
+                    Like_number = read.Like_number,
+                    Comment_numbe = read.Comment_numbe,
+                    Star = read.Star,
+                    Module = read.Module
+                });
+            }
+            list.Reverse();
+            return View(list); 
         }
     }
+
+
+
+
+
 }
